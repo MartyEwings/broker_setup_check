8ef46d51d785469d8ebe5dfa7a77c5a6bb6334d5
martyewings <martyewings@gmail.com>
2023-04-03 13:44:45 +0100

    Dev commits ignore

--- plans/broker_setup_check.pp
+++ plans/broker_setup_check.pp
@@ -6,5 +6,8 @@ plan broker_setup_check::broker_setup_check(
   # Build a PQL query to find all primary nodes
   $primary_node = puppetdb_query('inventory[certname]{ facts.pe_status_check_role = "primary" }').map |$r| { $r['certname'] }
 
-  return "does this contain anything:  ${broker_results}"
+  $broker_results.each |$compilers, $result| {
+    $broker_name = $result['value']['broker_uri']
+    notice("The broker for compiler ${compiler} is ${broker_uri}")
+  }
 }
