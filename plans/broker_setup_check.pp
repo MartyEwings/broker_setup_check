708f9128295ca18eced842c14270ed32916e714c
martyewings <martyewings@gmail.com>
2023-04-03 12:12:02 +0100

    Dev commits ignore

--- plans/broker_setup_check.pp
+++ plans/broker_setup_check.pp
@@ -1,22 +1,8 @@
 plan broker_setup_check::broker_setup_check(
-  Optional[TargetSpec] $targets          = undef,
+  TargetSpec $compilers = puppetdb_query('inventory[certname]{ facts.pe_status_check_role = "pe_complier" }').map |$r| { $r['certname'] },
 ) {
-  # Query PuppetDB if $targets is not unspecified
-  $_targets = if $targets =~ Undef {
-    $certnames_or_error = catch_errors() || {
-      puppetdb_query('inventory[certname]{ facts.pe_status_check_role = "pe_complier" }').map |$r| { $r['certname'] }
-    }
-    if $certnames_or_error =~ Error {
-      fail_plan("PuppetDB query failed: ${certnames_or_error}")
-    }
-    get_targets($certnames_or_error)
-  } else {
-    get_targets($targets)
-  }
-
-
   # Run the task against all compiler nodes
-  $broker_results = run_task('broker_setup_check::get_agent_broker', $_targets)
+  $broker_results = run_task('broker_setup_check::get_agent_broker', $compilers)
 
   # Build a PQL query to find all primary nodes
   $primary_nodes = puppetdb_query('inventory[certname]{ facts.pe_status_check_role = "primary" }').map |$r| { $r['certname'] }
