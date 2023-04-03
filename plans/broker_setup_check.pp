155ea13994d492301db024c54f62e1150566c5f4
martyewings <martyewings@gmail.com>
2023-04-03 12:34:12 +0100

    Dev commits ignore

--- plans/broker_setup_check.pp
+++ plans/broker_setup_check.pp
@@ -6,22 +6,4 @@ plan broker_setup_check::broker_setup_check(
 
   # Build a PQL query to find all primary nodes
   $primary_nodes = puppetdb_query('inventory[certname]{ facts.pe_status_check_role = "primary" }').map |$r| { $r['certname'] }
-
-  $all_ok = true
-
-  # Iterate over each target and compare its broker results with primary nodes
-  $compilers.each |$target| {
-    if $broker_results[$target] == $primary_nodes {
-      notice("Broker setup check passed for ${target}")
-    } else {
-      notice("Broker setup check failed for ${target}")
-      $all_ok = false
-    }
-  }
-
-  if $all_ok {
-    notice('Broker setup check passed for all targets')
-  } else {
-    fail('Broker setup check failed for one or more targets')
-  }
 }
