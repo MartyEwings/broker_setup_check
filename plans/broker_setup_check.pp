3679970df8a82ad5d29fdcca71ff2d3f64a11b4f
martyewings <martyewings@gmail.com>
2023-04-03 11:50:20 +0100

    Dev commits ignore

--- plans/broker_setup_check.pp
+++ plans/broker_setup_check.pp
@@ -20,7 +20,7 @@ plan broker_setup_check::broker_setup_check(
   $broker_results = run_task('get_agent_broker', $_targets)
 
   # Build a PQL query to find all primary nodes
-  $primary_nodes = query_nodes($primary_nodes_query)
+  $primary_nodes = puppetdb_query('inventory[certname]{ facts.pe_status_check_role = "primary" }').map |$r| { $r['certname'] }
 
   # Extract the broker string from the task results
   $broker_strings = $broker_results.map |$node, $result| { $result['broker_string'] }
