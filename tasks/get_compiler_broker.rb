abb16195e8359c617bf1fc0239884f2400050104
martyewings <martyewings@gmail.com>
2023-04-03 11:10:46 +0100

    first commit

---
+++ tasks/get_compiler_broker.rb
@@ -0,0 +1,19 @@
+require 'json'
+
+# Read the JSON data from the file
+conf_data = File.read('/etc/puppetlabs/pxp-agent/pxp-agent.conf')
+
+# Parse the JSON data
+conf_json = JSON.parse(conf_data)
+
+# Extract the broker URI
+broker_uri = conf_json['broker-ws-uris'][0]
+
+# Create a hash with the broker URI
+result = { "broker_uri" => broker_uri }
+
+# Convert the hash to a JSON string
+result_json = result.to_json
+
+# Print the JSON string
+puts result_json
