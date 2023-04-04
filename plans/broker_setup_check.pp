d513af750796aea3745d0d1fbc7e3f46cee69c97
martyewings <martyewings@gmail.com>
2023-04-04 08:33:09 +0100

    Dev commits ignore

--- plans/broker_setup_check.pp
+++ plans/broker_setup_check.pp
@@ -4,9 +4,21 @@ plan broker_setup_check::broker_setup_check(
   # Run the task against all compiler nodes
   $broker_results = run_task('broker_setup_check::get_agent_broker', $compilers)
   # Build a PQL query to find all primary nodes
-  $primary_node = puppetdb_query('inventory[certname]{ facts.pe_status_check_role = "primary" }').map |$r| { $r['certname'] }
+  $primary_node = puppetdb_query('inventory[certname]{ facts.pe_status_check_role = "primary" }').map |$r| { $r['certname'] }[0]
 
-  $broker_results.results.each |$result| {
-    return "Result for node ${result.target}: ${result.value}"
+  $results = $broker_results.map |$result| {
+    $target = $result.target.name
+    $broker = $result.value['broker_uri']
+    if $result.ok {
+      {
+        'name'   => $target,
+        'broker' => $broker,
+        'valid'  => $broker == "wss://${primary_node}:8142/pcp2/"
+      }
+    } else {
+      out::message("${target} errored with a message: ${result.error.message}")
+    }
   }
+
+  return $results
 }
